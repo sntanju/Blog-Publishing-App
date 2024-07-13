4f51f446db12add500c771707540a5d0e1e0cfb3
Sanjida Nasrin Tanju <86721558+sntanju@users.noreply.github.com>
2024-07-13 15:31:14 +0600

    Implemented Email Verification

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/only-verified', function () {
+    return view('only-verified');
+ })->middleware(['auth', 'verified']);
+
 require __DIR__.'/auth.php';
